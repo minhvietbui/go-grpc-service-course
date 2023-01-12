1c08adcf4fd831e90db91234d59900eaa34c6c00
VietBM <vietbm9@gmail.com>
2023-01-12 10:34:47 +0700

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "log"
+
+func Run() error {
+	
+	// responsible for initializing and starting
+	// out gRPC server
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
